caedf995acf74e2dd5109cae11c039c82a240ae5
ReneRambow <bill12gate@gmail.com>
2024-03-06 14:06:49 +0100

    update input

--- wedstrijdteller.js
+++ wedstrijdteller.js
@@ -2,35 +2,29 @@ console.log("test");
 
 
 
-function handleStartButton(event){
+function handleStartButton(event) {
     console.log("you pressed the button");
     let naam1 = inputTeam1.value;
     let naam2 = inputTeam2.value;
-    
-    if (naam1 == "") {
-        alert("Vul een naam in bij team 1");
+
+    if (naam1 == '') {
+        alert("Voer naam 1 in!");
+        return;
     }
-    if (naam1 != "") {
-        nameTeam1.innerText = inputTeam1.value;
-        naamEEN = 1
+    if (naam2 == '') {
+        alert("Voer naam 2 in!");
+        return;
     }
 
-    if (naam2 == "") {
-        alert("Vul een naam in bij team 2");
-    }
-    if (naam2 != "") {
-        nameTeam2.innerText = inputTeam2.value;
-        naamTWEE = 1
-    }
+    nameTeam1.innerText = naam1;
+    nameTeam2.innerText = naam2;
 
-    if (naamEEN == naamTWEE) {
-        if (servingTeam1.checked == true){
-            alert("Team 1 begint")
-        }
-    
-        if (servingTeam2.checked == true){
-            alert("Team 2 begint")
-        }
+
+    if (servingTeam1.checked == true) {
+        alert("Team 1 begint")
+    }
+    if (servingTeam2.checked == true) {
+        alert("Team 2 begint")
     }
 
 }
